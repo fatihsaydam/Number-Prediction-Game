b64f7a7fea0b5b6faaac2b137023f5f80338723a
Fatih Saydam <51199899+fatihsaydam@users.noreply.github.com>
2019-06-19 19:11:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _140710Con_SayiTahminOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bilgisayarın rastgele tuttuğu bir sayıyı kullanıcının tahmin ederek bulabileceği bir uygulama.
+
+            Random rastgele = new Random();
+            int tutulan = rastgele.Next(100);
+            int tahmin, sayac = 0;
+            do
+            {
+                Console.Write("Tahmininiz :");
+                tahmin = Convert.ToInt32(Console.ReadLine());
+                if (tahmin < tutulan)
+                    Console.WriteLine("Daha büyük bir sayı giriniz.");
+                else if (tahmin > tutulan)
+                    Console.WriteLine("Daha küçük bir sayı giriniz.");
+                sayac++;
+            } while (tahmin != tutulan);
+
+            Console.Write("Tebrikler {0} kerede bildiniz", sayac);
+            Console.ReadLine();
+        }
+    }
+}
